119788157e0573bfa48674fb85aef7504850d71f
pannal <1359593+pannal@users.noreply.github.com>
2026-04-10 14:34:21 +0200

    libdovi: add cmv29_payload_end_bit for bit-precise CMv4.0 append
    Add patch to expose cmv29_payload_end_bit in DoviVdrDmData, reporting
    the exact bit position where CMv2.9 DM data ends in the RPU bitstream.
    Regenerate prebuilt arm tarball with patched libdovi.
    
    Patch based on cpm-code/CoreELEC. Adapted for libdovi 3.3.1 (function
    signature changed: vdr_dm_data_payload no longer takes final_length).

--- projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
@@ -1,5 +1,38 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
+#
+# Rebuilding from source after patching libdovi:
+#
+# The default prebuilt path uses a pre-compiled tarball. When source-patches/
+# modify the Rust source, the prebuilt must be regenerated:
+#
+#   CE_TOOLCHAIN="<CE_BUILD>/build.CoreELEC-Amlogic-ng.arm-21/toolchain"
+#   CE_CC="${CE_TOOLCHAIN}/bin/armv8a-libreelec-linux-gnueabihf-gcc"
+#   CE_AR="${CE_TOOLCHAIN}/bin/armv8a-libreelec-linux-gnueabihf-ar"
+#   RUST_TARGET="arm-unknown-linux-gnueabihf"
+#
+#   # Requires: rustup, cargo-c (cargo install cargo-c),
+#   #           rustup target add arm-unknown-linux-gnueabihf
+#   export CC_arm_unknown_linux_gnueabihf="$CE_CC"
+#   export AR_arm_unknown_linux_gnueabihf="$CE_AR"
+#   export CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER="$CE_CC"
+#
+#   # Download, extract, and patch:
+#   wget https://github.com/quietvoid/dovi_tool/archive/libdovi-3.3.1.tar.gz
+#   tar xf libdovi-3.3.1.tar.gz && cd dovi_tool-libdovi-3.3.1
+#   patch -p1 < /path/to/source-patches/01-libdovi-store-cmv29-payload-end-bit.patch
+#
+#   # Build and install to staging directory:
+#   cargo cinstall --manifest-path dolby_vision/Cargo.toml \
+#     --target $RUST_TARGET --library-type staticlib \
+#     --profile release --prefix /usr --destdir /tmp/libdovi-install
+#
+#   # Package with required prefix (must match libdovi-${ARCH}-${PKG_VERSION}):
+#   mkdir -p /tmp/libdovi-pkg/libdovi-arm-3.3.1
+#   cp -a /tmp/libdovi-install/usr /tmp/libdovi-pkg/libdovi-arm-3.3.1/
+#   tar cJf libdovi-arm-3.3.1.tar.xz -C /tmp/libdovi-pkg libdovi-arm-3.3.1
+#   cp libdovi-arm-3.3.1.tar.xz <CE_SRC>/sources/libdovi/
+#   # Update PKG_SHA256 for "arm" below with: sha256sum libdovi-arm-3.3.1.tar.xz
 
 PKG_NAME="libdovi"
 PKG_VERSION="3.3.1"
@@ -12,7 +45,7 @@ if [ "${BUILD_FROM_SRC}" = "yes" ]; then
 else
   case "${TARGET_ARCH}" in
     "arm")
-      PKG_SHA256="9dc360de883c627014e8f36fe92950033e49ade1f94d596f58f0b26aab978fe1"
+      PKG_SHA256="7c0b0af11aa1919942ade07737d81cbb5b97ceb2d3220b68f87d58c034c181c3"
       ;;
     "aarch64")
       PKG_SHA256="e6e0bb82198a58a58cd38bbb2a6d286ff9d024ad35f490ff4b127ea415521457"
